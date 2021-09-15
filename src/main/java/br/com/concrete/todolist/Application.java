728d534d9f2a7bb48fda809e3f29ebbd676c8536
Jorge Rabello <jorge.rabello3@gmail.com>
2021-09-15 14:36:52 -0300

    chores: first commit

---
+++ src/main/java/br/com/concrete/todolist/Application.java
@@ -0,0 +1,7 @@
+package br.com.concrete.todolist;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
---
+++ src/test/java/br/com/concrete/todolist/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.concrete.todolist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void main() {
+
+    }
+}
