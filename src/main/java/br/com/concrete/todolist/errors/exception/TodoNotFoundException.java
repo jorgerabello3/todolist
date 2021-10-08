3da6816d478df3f82bedd91b5887984c4fc446d1
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-10-08 11:09:26 -0300

    Removido anotação

--- src/main/java/br/com/concrete/todolist/errors/exception/TodoNotFoundException.java
+++ src/main/java/br/com/concrete/todolist/errors/exception/TodoNotFoundException.java
@@ -1,10 +1,6 @@
 package br.com.concrete.todolist.errors.exception;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class TodoNotFoundException extends RuntimeException {
     public TodoNotFoundException(String message) {
         super(message);
